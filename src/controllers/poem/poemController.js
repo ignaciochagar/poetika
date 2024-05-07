3960978ce6a40444db40598edf17e3c4354fab8b
ignaciochagar <you@example.com>
2024-05-08 01:32:14 +0200

    Conexion con DB echa funcionando

--- src/controllers/poem/poemController.js
+++ src/controllers/poem/poemController.js
@@ -1,78 +1,74 @@
-const poems = [
-	{
-		"poem_id" : 1,
-		"title" : "Oda al viento",
-		"author" : "Pablo Neruda",
-		"year_release" : null
-	},
-	{
-		"poem_id" : 2,
-		"title" : "La divina comedia",
-		"author" : "Dante Alighieri",
-		"year_release" : null
-	},
-	{
-		"poem_id" : 3,
-		"title" : "Cien sonetos de amor",
-		"author" : "Pablo Neruda",
-		"year_release" : null
-	}
-];
 
-async function getAll(){
-    return {data: poems};
+import poemModel from "../../models/poemModel.js";
+
+async function getAll() {
+    try {
+        const poems = await poemModel.findAll();
+        return { data: poems };
+    }
+    catch (error) {
+        console.error(error);
+        return { error: error };
+    }
 }
 
-async function getById(id){
-    const poem = poems.find(poem => poem.poem_id === id);
-    if(!poem){
-        return {error: "El poema no existe"};
+async function getById(id) {
+    try {
+        const poem = await poemModel.findByPk(id);
+        if (!poem) {
+            return { error: "El poema no existe" };
+        }
+        return { data: poem };
     }
-    return {data: poem};
+    catch (error) {
+        console.error(error);
+        return { error };
+    }
 }
 
-async function create(poemData){
-    const {title, author, year_release} = poemData;
-    if(!title || !author){
-        return {error: "Los poemas deben tener título y autor"};
+async function create(poemtData) {
+    try {
+        const newPoem = await poemModel.create(poemData);
+        console.log("new poem", newPoem);
+        return { data: newPoem };
+    } catch (error) {
+        console.error(error);
+        return { error }
     }
-    const maxId = Math.max(...poems.map(poem => poem.poem_id));
-    const newId = maxId + 1;
-    const newPoem = {
-        poem_id: newId,
-        title,
-        author,
-        year_release
-    };
-    poems.push(newPoem);
-    return {data: newPoem};
 }
 
-async function update(id, poemData){
-    const {title, author, year_release} = poemData;
-    const poem = poems.find(poem => poem.poem_id === id);
-    if(!poem){
-        return {error: "No se puede modificar un poema que no existe"};
+async function update(id, poemData) {
+    try {
+        const { title, author, year_release } = poemData;
+        const poem = await poemModel.findByPk(id);
+        if (!poem) {
+            return { error: "No se puede modificar un poema que no existe, dartacan!" };
+        }
+        if (title) {
+            poem.title = title;
+        }
+        if (author) {
+            poem.author = author
+        }
+        if (year_release) {
+            poem.year_release = year_release;
+        }
+        const newPoem = await poemModel.update(id, poem);
+        return { data: newPoem };
+    } catch (error) {
+        console.error(error);
+        return { error }
     }
-    if(title){
-        poem.title = title;
-    }
-    if(author){
-        poem.author = author;
-    }
-    if(year_release){
-        poem.year_release = year_release;
-    }
-    return {data: poem};
+
 }
 
-async function remove(id){
-    const poemIndex = poems.findIndex(poem => poem.poem_id === id);
-    if(poemIndex === -1){
-        return {error: "No se pueden borrar poemas que no existen"};
+async function remove(id) {
+    try {
+        const result = await poemModel.remove(id);
+        return { data: result };
+    } catch (error) {
+        console.error(error);
     }
-    const deletedPoem = poems.splice(poemIndex, 1);
-    return {data: deletedPoem};
 }
 
 export {
