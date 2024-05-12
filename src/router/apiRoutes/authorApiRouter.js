bd56630676f66af51ffc83b383648a4df4587722
ivan-carballo <149379998+ivan-carballo@users.noreply.github.com>
2024-05-12 13:47:38 +0200

    Actualizar authorApiRouter.js

--- src/router/apiRoutes/authorApiRouter.js
+++ src/router/apiRoutes/authorApiRouter.js
@@ -5,7 +5,7 @@ import authorApiController from "../../controllers/author/authorApiController.js
 const router = Router();
 
 
-router.get("/",authorApiController.getAll);
+router.get("/",authorViewController.getAll);
 router.get("/create",authorApiController.create);
 router.get("/:id",authorApiController.getById);
 router.get("/:id/update",authorApiController.update);
